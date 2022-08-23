a6fde7ede796adcf291ea1987879b18c76340952
Senbadejo Oluwatola <badejooluwatola@gmail.com>
2022-08-23 11:33:43 +0100

    static pages ready

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,16 @@
-const {DateTime}
+const { DateTime } = require("luxon")
 
 
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css')
     eleventyConfig.addPassthroughCopy('./src/assets')
-
+    
+    eleventyConfig.addFilter('postDate', (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+    });    
      return{
-        dir: {
+        dir: {    
             input : "src",
             output: "public"
         }
